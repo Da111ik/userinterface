d7a47c4f304f0a4bb10f0b9a3ecee149c237a1d5
da111ik <6431817@gmail.com>
2020-07-02 23:14:44 +0300

    user sing in

---
+++ userinterface/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  userinterface
+//
+//  Created by Дарья Шимко on 01.07.2020.
+//  Copyright © 2020 Дарья Шимко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func singIn(_sender: UIButton) {
+        
+        
+    }
+
+}
+
