bdf5973b1f927165b5a2dc7d6a2d8b4e05d26cc5
da111ik <6431817@gmail.com>
2020-07-03 18:36:46 +0300

    keyboard

--- userinterface/ViewController.swift
+++ userinterface/ViewController.swift
@@ -12,17 +12,61 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var loginText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapGesture))
+        view.addGestureRecognizer(tapGesture)
         // Do any additional setup after loading the view.
     }
 
-  
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+ 
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func tapGesture() {
+        loginText.resignFirstResponder()
+        passwordText.resignFirstResponder()
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    @objc func keyboardWillHidden(notification: Notification) {
+     
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        
+    }
+
+    
     @IBAction func singIn(_sender: UIButton) {
         
         
     }
-
+    
 }
 
